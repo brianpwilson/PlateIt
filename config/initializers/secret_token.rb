e1a4128dd79ba025a9326115b26f8ed3e58b4d92
unknown <Kelley@.(none)>
2010-10-02 13:18:59 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlateIt::Application.config.secret_token = 'fc5939de14e485889fd88019d87a8077f8b4edffc87f8772b0283254ecaee45b160ffe7c93018393ad9da8d8bb02c0533dbeac8d10a45f9e3566a92f7e9f16e0'
